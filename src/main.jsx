5e84491964dfc947d0305bc2b617f976e79d5d81
César Arce <cesar.andres2139@gmail.comend>
2024-02-22 11:30:01 -0300

    Primer commint y NavBar

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Clients from "./pages/clients/Clients";
 import PageNotFound from "./pages/PageNotFound";
 import Layout from "./components/layout/Layout";
 
-const router = createBrowserRouter([{
-  path:"/",
-  element:<Layout/>,
-  errorElement:<PageNotFound/>,
-  children:[
-    {
-      path:"home",
-      element:<Home/>
-    },
-  ]
-}]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Ruta raíz */}
+          <Route path="/home" element={<Home />} /> {/* Ruta de la página de inicio */}
+          <Route path="/clients" element={<Clients />} /> {/* Ruta de clientes */}
+          <Route path="*" element={<PageNotFound />} /> {/* Ruta de página no encontrada */}
+        </Routes>
+      </Layout>
+    </Router>
   </React.StrictMode>
 );
---
+++ src/pages/clients/Clients.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Clients = () => {
+  return (
+    <div><h1>Aca estaran los Clientes</h1></div>
+  )
+}
+
+export default Clients;
