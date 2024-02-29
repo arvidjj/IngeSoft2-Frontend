e555a8614631e463625fe2df258a2713ffbd4a3a
Luz Lamas <144249539+llamaz01@users.noreply.github.com>
2024-02-28 22:13:53 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import InfoClients from "./pages/clients/InfoClients";
 import PageNotFound from "./pages/PageNotFound";
 import Layout from "./components/layout/Layout";
+import MainClients from "./pages/clients/mainClients";
 import Login from "./pages/Login";
 
 createRoot(document.getElementById("root")).render(
@@ -13,7 +14,8 @@ createRoot(document.getElementById("root")).render(
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Layout><Home /></Layout>} />
-          <Route path="/clientes" element={<Layout><InfoClients /></Layout>} />
+          <Route path="/clientes" element={<Layout><MainClients /></Layout>} />
+          <Route path="/clientesinfo" element={<Layout><InfoClients /></Layout>} />
           <Route path="*" element={<Layout><PageNotFound /></Layout>} />
         </Routes>
       </Router>
