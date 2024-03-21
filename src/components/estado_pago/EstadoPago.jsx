39ab695c0c100dfc66b74e412a79b4d0b38cd04b
Luz Lamas <luz.lamas2019@fiuni.edu.py>
2024-03-20 23:56:56 -0300

    arreglo de plan de pago y filtros

--- src/components/estado_pago/EstadoPago.jsx
+++ src/components/estado_pago/EstadoPago.jsx
@@ -3,38 +3,40 @@ import React from 'react';
 const EstadoPago = ({ estado }) => {
   let color;
   let textColor;
-  let estadoCapitalizado = estado.charAt(0).toUpperCase() + estado.slice(1).toLowerCase(); // Capitalizamos la primera letra y convertimos el resto a minúscula
+  let estadoCapitalizado;
 
-  // Convertimos el estado a minusculas para evitar fallos 
-  const estadoLowerCase = estado.toLowerCase();
+  if (estado) {
+    estadoCapitalizado = estado.charAt(0).toUpperCase() + estado.slice(1).toLowerCase(); // Capitalizamos la primera letra y convertimos el resto a minúscula
+  } else {
+    estadoCapitalizado = "Sin plan";
+  }
 
-  switch (estadoLowerCase) {
+  switch (estado) {
     case 'pagado':
       color = "#ECFDF3"; 
-      textColor = "#027A48";// Color verde 
+      textColor = "#027A48"; // Color verde 
       break;
     case 'pendiente':
       color = '#FFDBD9';
       textColor = '#D92D20'; // Color rojo 
       break;
     default:
-      color = 'black'; 
-      textColor = 'black';
+      color = '#FFDBD9'; 
+      textColor = '#D92D20'; // Color rojo por defecto
   }
-
 
   return (
     <div 
-    style={{
-      backgroundColor: color,
-      color: textColor,
-      borderRadius: "5px",
-      fontWeight: "bold",
-      textAlign: "center",
-      width: "65%",
-      marginLeft: "auto",
-      marginRight: "auto"
-    }}>
+      style={{
+        backgroundColor: color,
+        color: textColor,
+        borderRadius: "5px",
+        fontWeight: "bold",
+        textAlign: "center",
+        width: "65%",
+        marginLeft: "auto",
+        marginRight: "auto"
+      }}>
       {estadoCapitalizado}
     </div>
   );
