021dfe2a1aa329cc9eabc9d9041198729b6ebefc
arvidjj <andy.oj14@gmail.com>
2024-03-25 20:32:26 -0400

    Trabajando en caja

--- src/pages/caja/MainCaja.jsx
+++ src/pages/caja/MainCaja.jsx
@@ -1,30 +1,68 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useForm } from "react-hook-form"
-import BotonCrear from "../../components/bottons/ButtonCrear";
+import { Btn } from "../../components/bottons/Button";
 import CartaPrincipal from "../../components/cartaPrincipal/CartaPrincipal";
 import './MainCaja.css'
 import FlechaAtras from "../../components/flechaAtras/FlechaAtras";
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { FormTextInput } from "../../components/forms/FormInputs";
+
 
 const MainCaja = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
 
     return (
         <>
             <CartaPrincipal>
-                <FlechaAtras />
-                <h1>Registrar Nueva Caja</h1>
-                <form>
-                    <label for="nombre">Nombre de la Caja</label>
-                    <input type="text" placeholder="nombre" {...register("nombre", { required: true, maxLength: 15, minLength: 5 })} />
-                    <label for="monto">Monto de la Caja</label>
-                    <input type="number" placeholder="monto" {...register("monto", { required: true, max: 10000000, min: 50000 })} />
-                    <BotonCrear
-                        onClick={handleSubmit(onSubmit)}
-                        text={"Registrar Caja"}
-                    />
-                </form>
+                <div className="d-flex align-items-center justify-content-center my-auto">
+                    <div className="d-flex flex-column p-4 py-5 card" style={{"width": "25rem", marginLeft: 0}}>
+                        <h1>Abrir caja</h1>
+                        <Formik
+                            initialValues={{
+                                nombre: '',
+                                monto: '',
+                            }}
+                            validationSchema={Yup.object({
+                                nombre: Yup.string()
+                                    .max(15, 'Debe tener 15 caracteres o menos')
+                                    .required('Requerido'),
+                                monto: Yup.string()
+                                    .max(20, 'Debe tener 20 caracteres o menos')
+                                    .required('Requerido'),
+                            })}
+                            onSubmit={(values, { setSubmitting }) => {
+                                setTimeout(() => {
+                                    alert(JSON.stringify(values, null, 2));
+                                    setSubmitting(false);
+                                }, 400);
+                            }}
+                        >
+                            <Form>
+                                <div className="d-flex flex-column gap-2">
+                                    <FormTextInput
+                                        label="Nombre"
+                                        name="nombre"
+                                        type="text"
+                                        placeholder="Caja 1"
+                                        required={true}
+                                    />
+                                    <FormTextInput
+                                        label="Monto"
+                                        name="monto"
+                                        type="text"
+                                        placeholder="2000000"
+                                        required={true}
+                                    />
+                                    <Btn type="submit" className='mt-3 align-self-end'>
+                                        Submit
+                                    </Btn>
+                    
+                                </div>
+                            </Form>
+                        </Formik>
+                    </div>
+                </div>
+
             </CartaPrincipal>
         </>
     )
