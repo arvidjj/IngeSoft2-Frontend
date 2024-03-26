bb49965ce102496b45cb4dbfa9ff5e1dbc6487ba
CesarArceZ <cesar.andres2139@gmail.com>
2024-03-26 07:35:05 -0400

    Mejora de la compras en caja

--- src/components/modals/FormularioCaja.jsx
+++ src/components/modals/FormularioCaja.jsx
@@ -5,6 +5,10 @@ import "react-datepicker/dist/react-datepicker.css";
 import LabelBase from "../labels/LabelBase";
 import ButtonCrear from "../bottons/ButtonCrear";
 import ButtonBasic from "../bottons/ButtonBasic";
+import DateTime from "react-datetime";
+import "react-datetime/css/react-datetime.css";
+
+
 const FormularioCaja = () => {
   return (
     <Formik
@@ -28,64 +32,65 @@ const FormularioCaja = () => {
       {({ errors, touched }) => (
         <Form>
           <div className="row mb-3">
-            <div className="col-md-6">
-              <div className="d-flex">
-                <div style={{ width: "70%" }}>
-                  <LabelBase htmlFor="fecha">Fecha</LabelBase>
-                  <Field
-                    name="fecha"
-                    as={DatePicker}
-                    className="form-control"
+            <div className="col-md-4">
+              <LabelBase htmlFor="fecha">Fecha</LabelBase>
+              <Field
+                name="fecha"
+                render={({ field }) => (
+                  <DateTime
+                    {...field}
+                    inputProps={{ className: "form-control" }}
                   />
-                  <ErrorMessage name="fecha" component="div" />
-                </div>
-                <div style={{ width: "50%" }}>
-                  <LabelBase htmlFor="modalidadPago">
-                    Modalidad de Pago
-                  </LabelBase>
-                  <Field
-                    name="modalidadPago"
-                    as="select"
-                    className="form-control"
-                  >
-                    <option value="contado">Contado</option>
-                    <option value="cuota">Cuota</option>
-                  </Field>
-                  <ErrorMessage name="modalidadPago" component="div" />
-                </div>
-              </div>
+                )}
+              />
+              <ErrorMessage name="fecha" component="div" />
             </div>
-            <div className="d-flex">
-              <div style={{ width: "40%", marginRight: "1.5rem" }}>
-                <LabelBase htmlFor="ruc">RUC</LabelBase>
-                <Field type="text" name="ruc" className="form-control" />
-                <ErrorMessage name="ruc" component="div" />
-              </div>
-              <div style={{ width: "60%" }}>
-                <LabelBase htmlFor="razonSocial">Razón Social</LabelBase>
-                <Field
-                  type="text"
-                  name="razonSocial"
-                  className="form-control"
-                />
-                <ErrorMessage name="razonSocial" component="div" />
-              </div>
+            <div className="col-md-3">
+              <LabelBase htmlFor="modalidadPago">Modalidad de Pago</LabelBase>
+              <Field
+                name="modalidadPago"
+                as="select"
+                className="form-control"
+              >
+                <option value="contado">Contado</option>
+                <option value="cuota">Cuota</option>
+              </Field>
+              <ErrorMessage name="modalidadPago" component="div" />
             </div>
+          </div>
 
-            <div>
+          <div className="row mb-3">
+            <div className="col-md-6">
+              <LabelBase htmlFor="ruc">RUC</LabelBase>
+              <Field type="text" name="ruc" className="form-control" />
+              <ErrorMessage name="ruc" component="div" />
+            </div>
+            <div className="col-md-6">
+              <LabelBase htmlFor="razonSocial">Razón Social</LabelBase>
+              <Field
+                type="text"
+                name="razonSocial"
+                className="form-control"
+              />
+              <ErrorMessage name="razonSocial" component="div" />
+            </div>
+          </div>
+
+          <div className="row mb-3">
+            <div className="col-md-12">
               <LabelBase htmlFor="direccion">Dirección</LabelBase>
               <Field type="text" name="direccion" className="form-control" />
               <ErrorMessage name="direccion" component="div" />
             </div>
           </div>
-          <hr />
+<hr />
           <div className="row mb-3">
-            <div className="col-md-2" style={{ width: "10%" }}>
+            <div className="col-md-1">
               <LabelBase htmlFor="cantidad">Cantidad</LabelBase>
               <Field type="number" name="cantidad" className="form-control" />
               <ErrorMessage name="cantidad" component="div" />
             </div>
-            <div className="col-md-2" style={{ width: "25%" }}>
+            <div className="col-md-3">
               <LabelBase htmlFor="producto">Producto</LabelBase>
               <Field name="producto" as="select" className="form-control">
                 <option value="">Seleccione...</option>
@@ -102,7 +107,7 @@ const FormularioCaja = () => {
               />
               <ErrorMessage name="precioUnitario" component="div" />
             </div>
-            <div className="col-md-2" style={{ width: "10%" }}>
+            <div className="col-md-1">
               <LabelBase htmlFor="iva">IVA</LabelBase>
               <Field type="number" name="iva" className="form-control" />
               <ErrorMessage name="iva" component="div" />
@@ -112,13 +117,10 @@ const FormularioCaja = () => {
               <Field type="number" name="subtotal" className="form-control" />
               <ErrorMessage name="subtotal" component="div" />
             </div>
-            <div className="col">
-              <div className="d-flex justify-content-end mt-3">
-                <ButtonBasic id="btn-buscar" text="Agregar Item" />
-              </div>
+            <div className="col-md-3 d-flex align-items-end justify-content-end">
+                  <ButtonBasic text={"Agregar Items"}/>
             </div>
           </div>
-
           <div className="mb-3">
             <hr />
             <table className="table">
@@ -134,13 +136,13 @@ const FormularioCaja = () => {
               </thead>
             </table>
           </div>
-
           <div className="text-end">
             <ButtonCrear
               id="btn-crear"
               text="Generar Factura"
               color="secondary"
             />
+
           </div>
         </Form>
       )}
